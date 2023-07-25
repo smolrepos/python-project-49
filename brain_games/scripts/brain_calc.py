82ae75a46f315bb23bc8489dbadf5e7c09d861ab
antonsmolko <asmolko@yandex.ru>
2023-07-25 18:26:39 +0300

    fix complete3

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,35 +1,37 @@
-from ..cli import welcome_user
 import prompt
 from random import randint, choice
+from ..cli import welcome_user
+from brain_games.scripts.config import brain_calc as config
 
 
 name = welcome_user()
+OPERATIONS = config.OPERATIONS
+START_NUMBER = 1
+END_NUMBER = 100
+QUESTIONS_COUNT = 3
 print('What is the result of the expression?')
 
 
 def calculate(number_1, number_2, operation):
     match operation:
-        case '+':
+        case config.MATH_ADD:
             return number_1 + number_2
-        case '-':
+        case config.MATH_SUB:
             return number_1 - number_2
-        case _:
+        case config.MATH_MUL:
             return number_1 * number_2
-
-math_signs = ['+', '-', '*']
-start_number = 1
-end_number = 100
+        case _:
+            raise Exception(f'Wrong operation "{operation}".  Supported operation - {", ".join(OPERATIONS)}.')
 
 
 def main():
     count = 1
-    questions_count = 3
     complete = False
 
-    while count <= questions_count:
-        operation = choice(math_signs)
-        number_1 = randint(start_number, end_number)
-        number_2 = randint(start_number, end_number)
+    while count <= QUESTIONS_COUNT:
+        operation = choice(OPERATIONS)
+        number_1 = randint(START_NUMBER, END_NUMBER)
+        number_2 = randint(START_NUMBER, END_NUMBER)
         print(f'Question: {number_1} {operation} {number_2}')
         user_answer = prompt.string('Your answer: ')
         correct_answer = calculate(number_1, number_2, operation)
@@ -37,7 +39,7 @@ def main():
         if int(user_answer) == correct_answer:
             print('Corect!')
 
-            complete = count >= questions_count
+            complete = count >= QUESTIONS_COUNT
             count += 1
         else:
             print(f'Question: {number_1} {operation} {number_2}\nYour answer: {str(correct_answer)}')

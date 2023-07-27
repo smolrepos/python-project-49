cb06d4061c2149dca3248d71fe82bded790fab6a
smolrepos <140253912+smolrepos@users.noreply.github.com>
2023-07-27 14:50:29 +0300

    step nine complete (#6)
    * step nine complete

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,34 @@
+from random import randint
+from brain_games.engine import play
+
+
+START_NUMBER = 1
+END_NUMBER = 100
+
+
+def is_prime(number):
+  if number < 2:
+    return False
+  if number % 2 == 0:
+    return number == 2
+  if number % 3 == 0:
+    return number == 3
+  i = 5
+  while i ** 2 <= number:
+    if number % i == 0 or number % i + 2 == 0:
+      return False
+    i += 6
+  return True
+
+
+def round_generate():
+    question = randint(START_NUMBER, END_NUMBER)
+    correct_answer = 'yes' if is_prime(question) else 'no'
+
+    return [question, correct_answer]
+
+
+
+def main():
+    description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    play(round_generate, description)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,5 @@
+from brain_games.games.brain_prime import main
+
+
+if __name__ == '__main__':
+    main()
